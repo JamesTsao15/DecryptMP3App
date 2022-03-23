40719a8b2745153aaa269327df87b04850f19ed3
JamesTsao15 <jamestsao@gmail.com>
2022-03-23 20:16:19 +0800

    讀取不到dir內的資料

--- app/src/main/java/com/example/decryptmp3app/MainActivity.kt
+++ app/src/main/java/com/example/decryptmp3app/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.decryptmp3app
 
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -12,36 +11,20 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_WRITE_PERMISSION=100
+    private val REQUEST_WRITE_AND_READ_PERMISSION=100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestReadPermission()
+        val readPermisson:String=android.Manifest.permission.READ_EXTERNAL_STORAGE
+        val writePermission:String=android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if(ActivityCompat.checkSelfPermission(this,readPermisson)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(readPermisson,writePermission),REQUEST_WRITE_AND_READ_PERMISSION)
+        }
+        else{
             openKEYFile()
         }
     }
-    private fun requestReadPermission(){
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
-            val write_permission=android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-            val read_permission=android.Manifest.permission.READ_EXTERNAL_STORAGE
-            ActivityCompat.requestPermissions(this,arrayOf(write_permission,read_permission), REQUEST_WRITE_PERMISSION);
-        }
-        else {
-            openKEYFile();
-        }
-    }
-
     private fun openKEYFile() {
         val document=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
         val key_File= File(document,"EncryptMP3_Key_Text")
@@ -51,4 +34,19 @@ class MainActivity : AppCompatActivity() {
         Log.e("JAMES", Arrays.toString(key_File.listFiles()))
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.isNotEmpty() && requestCode==REQUEST_WRITE_AND_READ_PERMISSION){
+            val result=grantResults[0]
+            if(result==PackageManager.PERMISSION_DENIED)
+                finish()
+            else{
+                openKEYFile()
+            }
+        }
+    }
 }
